b4d330493203f559954555d8717dddfd328fa903
Romeo Balla <romeoballa15@gmail.com>
2026-02-19 23:15:19 +0100

    Security: Konfigurimi i skedarit .env dhe implementimi i enkriptimit Fernet

---
+++ api_engine/weather_service.py
@@ -0,0 +1,23 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_weather_data(city="Tirana"):
+    api_key = os.getenv("WEATHER_API_KEY")
+    # URL e API-së për motin
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    
+    try:
+        response = requests.get(url, timeout=5)
+        # Trajtimi i gabimeve të përgjigjeve (p.sh. 404 ose 500) [cite: 24]
+        if response.status_code == 200:
+            data = response.json()
+            temp = data['main']['temp']
+            desc = data['weather'][0]['description']
+            return f"{temp}°C, {desc}"
+        else:
+            return "Informacioni i motit nuk u gjet" # Trajtim i mungesës së të dhënave 
+    except Exception as e:
+        return f"Gabim gjatë lidhjes me API: {e}"
